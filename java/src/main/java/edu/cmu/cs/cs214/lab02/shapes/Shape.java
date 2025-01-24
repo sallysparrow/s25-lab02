b74f7962b1df848fcd9424ff2f6bc3029028080a
sallysparrow <chrome_cat@outlook.com>
2025-01-24 16:13:51 -0500

    circle updated

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
@@ -5,3 +5,22 @@ public interface Shape {
     void draw();
 }
 
+public class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
--- typescript/src/shapes/circle.ts
+++ typescript/src/shapes/circle.ts
@@ -1,10 +1,21 @@
-function circle(radius: number): Shape {
-    return {
-        radius,
+import { Shape } from "./shape"
+
+class circle implements Shape {
+        private radius: number;
+
+        constructor(radius: number){
+            this.radius = radius;  
+        }
+
+        getArea(): number {
+            return Math.PI * this.radius * this.radius
+        }
+
+
         computeArea: function (): number {
             return Math.PI * radius * radius
         }
-    }
+=
 }
 
 export { circle }
